6f9aa3cd9780f01d1af7827b77223d449d9d4739
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-11-24 14:28:53 +0300

    Added Core

---
+++ JwtProject.Core/Abstract/IGenericDal.cs
@@ -0,0 +1,15 @@
+﻿using JwtProject.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace JwtProject.Core.Abstract;
+
+public interface IGenericDal<T> where T: class,IEntity,new()
+{
+    Task<List<T>> GetAllAsync(Expression<Func<T,bool>> predicate=null, params Expression<Func<T, object>>[] includeProp);
+    Task<T> GetAsync(Expression<Func<T,bool>> predicate=null, params Expression<Func<T, object>>[] includeProp);
+    Task<T> GetByIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+    Task DeleteAsync(int id);
+    Task<T> UpdateAsync(T entity);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> filter);
+}
---
+++ JwtProject.Core/Concrete/EfGenericRepository.cs
@@ -0,0 +1,86 @@
+﻿using JwtProject.Core.Abstract;
+using JwtProject.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace JwtProject.Core.Concrete;
+
+public class EfGenericRepository<TEntity> : IGenericDal<TEntity> where TEntity : class, IEntity, new()
+{
+    protected readonly DbContext _context;
+
+    public EfGenericRepository(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProp)
+    {
+        IQueryable<TEntity> query= _context.Set<TEntity>();
+        if(predicate != null)
+        {
+            query = query.Where(predicate);
+        }
+        if(includeProp !=null)
+        {
+            foreach(var include in includeProp)
+            {
+                query = query.Include(include);
+            }
+        }
+        return await query.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<TEntity> GetByIdAsync(int id)
+    {
+        var findId = await _context.Set<TEntity>().FindAsync(id);
+        return findId;
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> filter)
+    {
+        return await _context.Set<TEntity>().AnyAsync(filter);
+    }
+
+    public async Task<TEntity> CreateAsync(TEntity entity)
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        _context.Set<TEntity>().Update(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var findId= await _context.Set<TEntity>().FindAsync(id);
+        _context.Remove(findId);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProp)
+    {
+        IQueryable<TEntity> query = _context.Set<TEntity>();
+        if (predicate != null)
+        {
+            query = query.Where(predicate);
+        }
+
+        if (includeProp != null)
+        {
+            if (includeProp.Any())
+            {
+                foreach (var includeProperty in includeProp)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+        }
+        return await query.AsNoTracking().FirstOrDefaultAsync();
+    }
+}
